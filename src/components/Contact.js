0a67bde7adedcba4d5ebbaaded4c2f05c6e0fea2
Vinay Joshi <vinay01joshi@gmail.com>
2020-01-04 19:54:06 +0530

    Changing state from another component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,12 +11,22 @@ class Contact extends Component {
         this.setState({ showContactInfo: !this.state.showContactInfo })
     }
 
+    onDeleteClick = (e) =>{
+        this.props.deleteClickHandler();
+    }
+
     render() {
         const {name , email, phone} = this.props.contact ;
         const {showContactInfo} = this.state;
         return (
             <div className="card card-body mb-3">
-                <h4>{name} <i onClick={ this.onShowClick } className="fas fa-sort-down"></i></h4>
+                <h4>{name} 
+                    <i onClick={ this.onShowClick } className="fas fa-sort-down" 
+                        style={{ cursor: 'pointer' }}></i>
+                    <i className="fas fa-times" 
+                        style={{ cursor: 'pointer', float:'right' , color: 'red' }}
+                        onClick={ this.onDeleteClick } ></i>
+                </h4>
                 {showContactInfo ? (
                     <ul className="list-group">
                         <li className="list-group-item">Email: {email}</li>
@@ -29,7 +39,8 @@ class Contact extends Component {
 }
 
 Contact.propTypes = {
-    contact : PropTypes.object.isRequired
+    contact : PropTypes.object.isRequired,
+    deleteClickHandler :PropTypes.func.isRequired
 };
 
 export default Contact;
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -9,7 +9,19 @@ import Contact from './Contact';
                     {id: 2, name: "Tanuja Joshi" , email: "tanuja01joshi@gmail.com" , phone: '868-9896-858' },
                     {id: 3, name: "Tanvi Joshi" , email: "tanvi01joshi@gmail.com" , phone: '999-9896-858' },
                 ]
-            }
+            };
+    
+    deleteContact = (id) => {
+        console.log('Id which gonna be deleted', id);
+        const { contacts } = this.state;
+
+        const newContacts = contacts.filter( contact => contact.id !== id) ;
+
+        this.setState({
+            contacts : newContacts
+        })
+
+    }
 
     render() {
         const { contacts } = this.state;
@@ -18,7 +30,7 @@ import Contact from './Contact';
             // to do so we are using React.Fragment
             <React.Fragment>
                 { contacts.map(contact => (
-                    <Contact key={contact.id} contact={contact} />
+                    <Contact key={contact.id} contact={contact} deleteClickHandler={this.deleteContact.bind(this, contact.id)} />
                 ))}
                 
             </React.Fragment>

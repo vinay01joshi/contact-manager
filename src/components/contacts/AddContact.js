6bf5dbcc8f786e1e4305f8796145f998f9543dc8
Vinay Joshi <vinay01joshi@gmail.com>
2020-01-04 21:43:25 +0530

    Controlled component

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -6,6 +6,13 @@ class AddContact extends Component {
         name : '',
         email :'',
         phone : ''
+    };
+
+    onChange = e => this.setState({ [e.target.name]: e.target.value});
+
+    onSubmit = (e) =>{
+        e.preventDefault();
+        console.log(this.state);
     }
 
     render() {
@@ -14,7 +21,7 @@ class AddContact extends Component {
             <div className="card mb-3">
                 <div className="card-header">Add</div>
                 <div className="card-body">
-                    <form>
+                    <form onSubmit={this.onSubmit}>
                         <div className="form-group">
                             <label htmlFor="name">Name</label>
                             <input 
@@ -22,7 +29,8 @@ class AddContact extends Component {
                                 className="form-control form-control-lg"
                                 placeholder="Enter Name"
                                 name="name"
-                                value={name}></input>
+                                value={name}
+                                onChange={this.onChange}></input>
                         </div>
                         <div className="form-group">
                             <label htmlFor="email">Email</label>
@@ -31,7 +39,8 @@ class AddContact extends Component {
                                 className="form-control form-control-lg"
                                 placeholder="Enter Email"
                                 name="email"
-                                value={email}></input>
+                                value={email}
+                                onChange={this.onChange}></input>
                         </div>
                         <div className="form-group">
                             <label htmlFor="phone">Phone</label>
@@ -40,7 +49,8 @@ class AddContact extends Component {
                                 className="form-control form-control-lg"
                                 placeholder="Enter Phone"
                                 name="phone"
-                                value={phone}></input>
+                                value={phone}
+                                onChange={this.onChange}></input>
                         </div>
                         <input type="submit" value="Add"
                             className="btn btn-light btn-block"></input>

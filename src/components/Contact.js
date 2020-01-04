79ae854e00e593a93d351e8790104da60c6e138e
Vinay Joshi <vinay01joshi@gmail.com>
2020-01-04 19:30:18 +0530

    Changing Stage

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,21 +3,26 @@ import PropTypes from 'prop-types';
 
 class Contact extends Component {
 
-    state = {}
+    state = {
+        showContactInfo : false
+    }
 
     onShowClick = (e) => {
-        console.log('event sate ',e.target )
+        this.setState({ showContactInfo: !this.state.showContactInfo })
     }
 
     render() {
         const {name , email, phone} = this.props.contact ;
+        const {showContactInfo} = this.state;
         return (
             <div className="card card-body mb-3">
                 <h4>{name} <i onClick={ this.onShowClick } className="fas fa-sort-down"></i></h4>
-                <ul className="list-group">
-                    <li className="list-group-item">Email: {email}</li>
-                    <li className="list-group-item">Phone: {phone}</li>
-                </ul>
+                {showContactInfo ? (
+                    <ul className="list-group">
+                        <li className="list-group-item">Email: {email}</li>
+                        <li className="list-group-item">Phone: {phone}</li>
+                    </ul>
+                ) : null}                
             </div>
         )
     }

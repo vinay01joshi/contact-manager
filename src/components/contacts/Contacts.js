6a06aee2d89063f9858c65e926a876366955bf01
Vinay Joshi <vinay01joshi@gmail.com>
2020-01-05 19:18:53 +0530

    Introducing react routing

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -11,6 +11,11 @@ import {Consumer} from '../../Context'
                         // Replacing Div element with React.Fragment as we do not need extra div inside the contacts element
                         // to do so we are using React.Fragment
                         <React.Fragment>
+                            <h1 className="display-4 mb-2">
+                                <span className="text-primary">
+                                    Contact
+                                </span> List
+                            </h1>
                             { contacts.map(contact => (
                                 <Contact key={contact.id} contact={contact} />
                             ))}

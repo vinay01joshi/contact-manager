790b6ad9b3bf69e4bb4b7afbe92c4d6541280a96
Vinay Joshi <vinay01joshi@gmail.com>
2020-01-05 19:40:52 +0530

    adding new page -> Page not Found

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header'
 import AddContact from './components/contacts/AddContact'
 import About from './components/pages/About'
+import NotFound from './components/pages/NotFound'
 
 import {Provider} from './Context'
 
@@ -21,6 +22,7 @@ function App() {
               <Route exact path="/" component={Contacts}/>
               <Route exact path="/contact/add" component={AddContact}/>
               <Route exact path="/about" component={About}/>
+              <Route exact component={NotFound}/>
             </Switch>
           </div>      
         </div>

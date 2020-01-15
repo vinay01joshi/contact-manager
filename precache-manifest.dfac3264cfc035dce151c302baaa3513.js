d7833ea02e1ab95986a3a2739d410e76f0f2195f
Vinay Joshi <vinay01joshi@gmail.com>
2020-01-15 15:58:26 +0530

    Updates

---
+++ precache-manifest.dfac3264cfc035dce151c302baaa3513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1a7f04b9e1eb61a44c1c2879672fc2",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "84c7b3ff509b6c775a6f",
+    "url": "/contact-manager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3ff79437c2231ac8e3d7",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "84c7b3ff509b6c775a6f",
+    "url": "/contact-manager/static/js/2.daea2310.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contact-manager/static/js/2.daea2310.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ff79437c2231ac8e3d7",
+    "url": "/contact-manager/static/js/main.0525983e.chunk.js"
+  },
+  {
+    "revision": "cf2b1269387e28de406a",
+    "url": "/contact-manager/static/js/runtime-main.a5021be2.js"
+  }
+]);
